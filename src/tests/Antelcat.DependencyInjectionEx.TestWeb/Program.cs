c64299d745b01cd7de241631c6a4fbb0df91598f
feast107 <feast107@gmail.com>
2024-08-11 15:47:17 +0800

    Update Program.cs

--- src/tests/Antelcat.DependencyInjectionEx.TestWeb/Program.cs
+++ src/tests/Antelcat.DependencyInjectionEx.TestWeb/Program.cs
@@ -1,5 +1,10 @@
 using System.Diagnostics;
+using Antelcat.DependencyInjectionEx;
 using Antelcat.DependencyInjectionEx.TestWeb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +17,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Host.UseAutowiredServiceProviderFactory();
-    
+
 var app = builder.Build();
 
 var b = app.Services.GetRequiredService<IB>();
+
+(app.Services as ServiceProviderEx)!.ServiceConstructed += (p, type, controller, kind) =>
+{
+    if (controller is ControllerBase)
+    {
+        Debugger.Break();
+    }
+};
+
 Debug.Assert(b is B { A : not null });
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
